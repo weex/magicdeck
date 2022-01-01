eb08cc4424f2b0e97c153cc44cfe324eb729732d
David Sterry <davesterry@gmail.com>
2021-12-31 21:17:08 -0800

    interactive boxes now work

--- main.lua
+++ main.lua
@@ -16,27 +16,15 @@ function lovr.load()
   world = lovr.physics.newWorld(0, -2, 0, false) -- low gravity and no collider sleeping
   local box = world:newBoxCollider(vec3(0, 0, 0), vec3(20, 0.1, 20))
   box:setKinematic(true)
-end
 
-function lovr.update(dt)
-end
-
-function lovr.draw()
-  lovr.graphics.skybox(skybox)
-
-  -- create floor and walls
-  lovr.graphics.setColor(0x203166)
-  lovr.graphics.cylinder(0,-1,0, 3,  math.pi/2,  1,0,0,  10)
-  make_boxes(0.5, 4, 8, 0x304176)
-
-  for height = 0.3, 1.2, 0.3 do
-    local pose = mat4():translate(0, height, -1)
-    local size = vec3(0.3, 0.3, 0.2)
+  for height = 0.3, 1.5, 0.4 do
+    local pose = mat4():rotate(math.pi/8, 0,1,0):translate(0, height, -1)
+    local size = vec3(0.3, 0.4, 0.2)
     local box = world:newBoxCollider(vec3(pose), size)
     box:setOrientation(quat(pose))
     table.insert(boxes, box)
   end
-  
+
   -- make colliders for two hands
   for i = 1, 2 do
     hands.colliders[i] = world:newBoxCollider(vec3(0,2,0), vec3(0.04, 0.08, 0.08))
@@ -49,21 +37,87 @@ function lovr.draw()
         hands.touching[i] = collider
       end)
   end
+end
 
-  -- draw hands
-  lovr.graphics.setColor(0xd2b793)
-  for _, handName in ipairs(lovr.headset.getHands()) do
-    local skeleton = lovr.headset.getSkeleton(handName)
-    if skeleton then
-      for _, bone in ipairs(skeleton) do
-        lovr.graphics.sphere(mat4(unpack(bone)):scale(0.004, 0.002, 0.006))
+function lovr.update(dt)
+  -- override collision resolver to notify all colliders that have registered their callbacks
+  world:update(framerate, function(world)
+    world:computeOverlaps()
+    for shapeA, shapeB in world:overlaps() do
+      local areColliding = world:collide(shapeA, shapeB)
+      if areColliding then
+        cbA = collisionCallbacks[shapeA]
+        if cbA then cbA(shapeB:getCollider(), world) end
+        cbB = collisionCallbacks[shapeB]
+        if cbB then cbB(shapeA:getCollider(), world) end
       end
-    else
-      local handPose = mat4(lovr.headset.getPose(handName))
-      handPose:scale(0.04, 0.08, 0.12)
-      lovr.graphics.box('fill', handPose)
+    end
+  end)
+  -- hand updates - location, orientation, solidify on trigger button, grab on grip button
+  for i, hand in pairs(lovr.headset.getHands()) do
+    -- align collider with controller by applying force (position) and torque (orientation)
+    local rw = mat4(lovr.headset.getPose(hand))   -- real world pose of controllers
+    local vr = mat4(hands.colliders[i]:getPose()) -- vr pose of palm colliders
+    local angle, ax,ay,az = quat(rw):mul(quat(vr):conjugate()):unpack()
+    angle = ((angle + math.pi) % (2 * math.pi) - math.pi) -- for minimal motion wrap to (-pi, +pi) range
+    hands.colliders[i]:applyTorque(vec3(ax, ay, az):mul(angle * dt * 1))
+    hands.colliders[i]:applyForce((vec3(rw:mul(0,0,0)) - vec3(vr:mul(0,0,0))):mul(dt * 2000))
+    -- solidify when trigger touched
+    hands.solid[i] = lovr.headset.isDown(hand, 'trigger')
+    hands.colliders[i]:getShapes()[1]:setSensor(not hands.solid[i])
+    -- hold/release colliders
+    if lovr.headset.isDown(hand, 'grip') and hands.touching[i] and not hands.holding[i] then
+      hands.holding[i] = hands.touching[i]
+      lovr.physics.newBallJoint(hands.colliders[i], hands.holding[i], vr:mul(0,0,0))
+      lovr.physics.newSliderJoint(hands.colliders[i], hands.holding[i], quat(vr):direction())
+    end
+    if lovr.headset.wasReleased(hand, 'grip') and hands.holding[i] then
+      for _,joint in ipairs(hands.colliders[i]:getJoints()) do
+        joint:destroy()
+      end
+      hands.holding[i] = nil
     end
   end
+end
+
+function lovr.draw()
+  lovr.graphics.skybox(skybox)
+
+  -- create floor and walls
+  lovr.graphics.setColor(0x203166)
+  lovr.graphics.cylinder(0,-1,0, 3,  math.pi/2,  1,0,0,  10)
+  make_boxes(0.5, 4, 8, 0x304176)
+
+  for i, collider in ipairs(hands.colliders) do
+    local alpha = hands.solid[i] and 1 or 0.2
+    lovr.graphics.setColor(0.75, 0.56, 0.44, alpha)
+    drawBoxCollider(collider)
+  end
+  lovr.math.setRandomSeed(0)
+  for i, collider in ipairs(boxes) do
+    local shade = 0.2 + 0.6 * lovr.math.random()
+    lovr.graphics.setColor(shade, shade, shade)
+    drawBoxCollider(collider)
+  end
+end
+
+function drawBoxCollider(collider)
+  -- query current pose (location and orientation)
+  local pose = mat4(collider:getPose())
+  -- query dimensions of box
+  local shape = collider:getShapes()[1]
+  local size = vec3(shape:getDimensions())
+  -- draw box
+  pose:scale(size)
+  lovr.graphics.box('fill', pose)
+end
+
+function registerCollisionCallback(collider, callback)
+  collisionCallbacks = collisionCallbacks or {}
+  for _, shape in ipairs(collider:getShapes()) do
+    collisionCallbacks[shape] = callback
+  end
+  -- to be called with arguments callback(otherCollider, world) from update function
 end
 
 function make_boxes(height, width, distance, color)
